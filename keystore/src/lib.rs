b75a0bc39e7b87e9e116ef0b606a7312ae0a1000
tiannian <dtiannian@aliyun.com>
2020-12-18 00:15:40 +0800

    complete main fn for keystore

--- keystore/src/lib.rs
+++ keystore/src/lib.rs
@@ -1,4 +1,10 @@
-#[marrow::main]
+#![no_std]
+#![feature(default_alloc_error_handler)]
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[marrow_rt::main]
 async fn main() {
 
 }
