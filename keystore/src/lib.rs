4ba9f13a1b57b80d72cf521fc39e6155a51ca564
tiannian <dtiannian@aliyun.com>
2020-12-18 04:53:46 +0800

    fix basic mian

--- keystore/src/lib.rs
+++ keystore/src/lib.rs
@@ -1,11 +1,10 @@
-#[marrow::main]
+#![no_std]
+#![feature(default_alloc_error_handler)]
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[mw_rt::async_main]
 async fn main() {
 
-}
-
-#[no_mangle]
-pub extern "C" fn _entry() {
-    let runtime = runtime::Runtime::new();
-
-    runtime.spawn(main());
 }
