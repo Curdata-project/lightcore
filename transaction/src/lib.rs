e12d70178b9e297925e2f2d704f89e27792d6afc
tiannian <dtiannian@aliyun.com>
2020-12-21 11:13:24 +0800

    inital transaction

---
+++ transaction/src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![feature(default_alloc_error_handler)]
+extern crate alloc;
+// extern crate mw_rt;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[mw_rt::async_main]
+async fn main() {
+
+}
