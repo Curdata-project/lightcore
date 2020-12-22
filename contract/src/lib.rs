f2845253c1c951e2219eb5a93a457f3713331e05
tiannian <dtiannian@aliyun.com>
2020-12-22 15:18:00 +0800

    clean warning for cargo and strip wasm file size

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![no_std]
+#![feature(default_alloc_error_handler)]
+extern crate alloc;
+// extern crate mw_rt;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[mw_rt::async_main]
+async fn main() {
+
 }
