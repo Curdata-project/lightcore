53442de0563654a4334999970f0551f8c65ad153
tiannian <dtiannian@aliyun.com>
2020-12-22 16:36:32 +0800

    add index demo

--- index.js
+++ index.js
@@ -4,15 +4,43 @@ const modules = {
     keystore: {
         path: 'target/wasm32-unkonwn-unknown-release/release/keystore.wasm',
         deps: [],
-        // expose: [
-        //     'get_account',
-        // ],
+        expose: [
+            {
+                'type': 'callback',
+                'name': 'list_accounts',
+                'args': [
+                    { 'name': 'page', 'type': 'number' },
+                    { 'name': 'item', 'type': 'number' },
+                    { 'name': 'order', 'type': 'number' },
+                ],
+                'return': {
+                    'type': 'proto',
+                    'proto': 'keystore/proto/keystore.proto',
+                    'message': 'KeypairDisplayList'
+                }
+            },
+            {
+                'type': 'callback',
+                'name': 'get_account',
+                'args': [
+                    { 'name': 'account', 'type': 'bytes' },
+                ],
+                'return': {
+                    'type': 'proto',
+                    'proto': 'keystore/proto/KeypairDisplay'
+                }
+            },
+            {
+                'type': 'callback',
+                'name': 'new_account',
+                'args': [],
+                'return': {
+                    'type': 'proto',
+                    'proto': 'keystore/proto/KeypairDisplay'
+                }
+            },
+        ],
     },
-    // transcation: {
-    //     path: 'target/wasm32-unkonwn-unknown-release/release/transcation.wasm',
-    //     deps: ['keystore'],
-    // }
 }
 
-runtime.load(modules);
-runtime.run();
+runtime.run(modules);
