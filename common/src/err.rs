d447863393b0b48a309074f935e1ce1cd5420750
malefooo <784479660@qq.com>
2021-01-18 10:52:08 +0800

    fix:error code

--- common/src/err.rs
+++ common/src/err.rs
@@ -18,38 +18,38 @@ impl Err {
     pub fn get(self) -> (usize, String) {
         return match self {
             Err::ProtoErrors(e) => match e {
-                qp_errors::Io => (20001, "quick protobuf IO ERROR".to_string()),
+                qp_errors::Io => (20001, "20001:IO:quick protobuf IO ERROR".to_string()),
                 qp_errors::Utf8(err) => {
-                    (20002, alloc::format!("quick protobuf Utf8 ERROR:{:?}", err))
+                    (20002, alloc::format!("20002:Utf8:quick protobuf Utf8 ERROR:{:?}", err))
                 }
                 qp_errors::Deprecated(err) => (
                     20003,
-                    alloc::format!("quick protobuf Deprecated ERROR:{:?}", err),
+                    alloc::format!("20003:Deprecated:quick protobuf Deprecated ERROR:{:?}", err),
                 ),
                 qp_errors::UnknownWireType(err) => (
                     20004,
-                    alloc::format!("quick protobuf UnknownWireType ERROR:{:?}", err),
+                    alloc::format!("20004:UnknownWireType:quick protobuf UnknownWireType ERROR:{:?}", err),
                 ),
-                qp_errors::Varint => (20004, "quick protobuf VarInt ERROR".to_string()),
+                qp_errors::Varint => (20005, "20005:Varint:quick protobuf VarInt ERROR".to_string()),
                 qp_errors::Map(err) => {
-                    (20005, alloc::format!("quick protobuf Map ERROR:{:?}", err))
+                    (20006, alloc::format!("20006:Map:quick protobuf Map ERROR:{:?}", err))
                 }
                 qp_errors::UnexpectedEndOfBuffer => (
-                    20006,
-                    "quick protobuf UnexpectedEndOfBuffer ERROR".to_string(),
+                    20007,
+                    "20007:UnexpectedEndOfBuffer:quick protobuf UnexpectedEndOfBuffer ERROR".to_string(),
                 ),
                 qp_errors::OutputBufferTooSmall => (
-                    20006,
-                    "quick protobuf OutputBufferTooSmall ERROR".to_string(),
+                    20008,
+                    "20008:OutputBufferTooSmall:quick protobuf OutputBufferTooSmall ERROR".to_string(),
                 ),
             },
-            Err::FromUtf8Error(err) => (20007, alloc::format!("from utf8 ERROR:{}", err)),
-            Err::Null(err) => (20008, alloc::format!("Null ERROR:{:?}", err)),
-            Err::VaildFail(err) => (20009, alloc::format!("VaildFail ERROR:{:?}", err)),
-            Err::UnlockFail(err) => (20010, alloc::format!("UnlockFail ERROR:{:?}", err)),
-            Err::SqlExecture(err) => (20011, alloc::format!("SqlExecture ERROR:{:?}", err)),
-            Err::CallState(err) => (20012, alloc::format!("CallState ERROR:{:?}", err)),
-            Err::AccountErrors(err) => (20013, alloc::format!("Account ERROR:{:?}", err)),
+            Err::FromUtf8Error(err) => (20009, alloc::format!("20009:FromUtf8Error:from utf8 ERROR:{}", err)),
+            Err::Null(err) => (20010, alloc::format!("20010:Null:Null ERROR:{:?}", err)),
+            Err::VaildFail(err) => (20011, alloc::format!("20011:VaildFail:VaildFail ERROR:{:?}", err)),
+            Err::UnlockFail(err) => (20012, alloc::format!("20012:UnlockFail:UnlockFail ERROR:{:?}", err)),
+            Err::SqlExecture(err) => (20013, alloc::format!("20013:SqlExecture:SqlExecture ERROR:{:?}", err)),
+            Err::CallState(err) => (20014, alloc::format!("20014:CallState:CallState ERROR:{:?}", err)),
+            Err::AccountErrors(err) => (20015, alloc::format!("20015:AccountErrors:Account ERROR:{:?}", err)),
         };
     }
 }
